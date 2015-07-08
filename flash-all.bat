c30479ef5be8ed313f55fd7cd7f130b9d6ed98f4
DooMLoRD <metoo.mememe@gmail.com>
2015-07-08 15:47:57 +0200

    update steps for YUPHORIA

--- flash-all.bat
+++ flash-all.bat
@@ -1,26 +1,29 @@
 @echo ---------------------[ Start flashing ROM ]-------------------------
 @echo ----------------[ Checking for connected devices ]------------------
-@fastboot -i 0x1ebf devices 
+@fastboot -i 0x2A96 devices 
 @echo -------------------[ Unlocking bootloader ]-------------------------
-@fastboot -i 0x1ebf oem unlock 
-@echo ---------------[ Flashing modem and bootloader ]--------------------
-@fastboot -i 0x1ebf flash aboot emmc_appsboot.mbn
-@fastboot -i 0x1ebf flash modem NON-HLOS.bin
-@fastboot -i 0x1ebf flash rpm rpm.mbn
-@fastboot -i 0x1ebf flash sbl1 sbl1.mbn
-@fastboot -i 0x1ebf flash tz tz.mbn
-@fastboot -i 0x1ebf flash hyp hyp.mbn
+@fastboot -i 0x2A96 oem unlock 
 @echo ----------------------[ Flashing kernel ]---------------------------
-@fastboot -i 0x1ebf flash boot boot.img
-@echo ----------------------[ Flashing recovery ]-------------------------
-@fastboot -i 0x1ebf flash recovery recovery.img
-@echo -------------------[ Flashing system partition ]--------------------
-@fastboot -i 0x1ebf flash system system.img
+@fastboot -i 0x2A96 flash boot boot.img
+@echo ---------------[ Flashing modem and bootloader ]--------------------
+@fastboot -i 0x2A96 flash aboot emmc_appsboot.mbn
+@fastboot -i 0x2A96 flash modem NON-HLOS.bin
+@fastboot -i 0x2A96 flash rpm rpm.mbn
+@fastboot -i 0x2A96 flash sbl1 sbl1.mbn
+@fastboot -i 0x2A96 flash tz tz.mbn
+@fastboot -i 0x2A96 flash hyp hyp.mbn
+@fastboot -i 0x2A96 flash splash splash.img
+@echo -------------------[ Flashing cache partition ]---------------------
+@fastboot -i 0x2A96 flash cache cache.img
 @echo -------------------[ Flashing data partition ]----------------------
-@fastboot -i 0x1ebf flash userdata userdata.img
+@fastboot -i 0x2A96 flash userdata userdata.img
+@echo -------------------[ Flashing system partition ]--------------------
+@fastboot -i 0x2A96 flash system system.img
+@echo ----------------------[ Flashing recovery ]-------------------------
+@fastboot -i 0x2A96 flash recovery recovery.img
 @echo ---------------------[ Relocking bootloader ]-----------------------
-@fastboot -i 0x1ebf oem lock
+@fastboot -i 0x2A96 oem lock
 @echo ---------------------[ Rebooting device ]---------------------------
-@fastboot -i 0x1ebf reboot
+@fastboot -i 0x2A96 reboot
 @echo -------------------------[ ALL DONE ]-------------------------------
 
